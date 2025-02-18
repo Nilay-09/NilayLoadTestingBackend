6781e68b62790a0022d0c621b9a8637ff32e75c6
Nilay-09 <nilaybhotmange2002@gmail.com>
2025-02-18 10:51:19 +0530

    Add CORS middleware and requirements for FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from app.api.config.config import config_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Load Testing Tool", version="1.0")
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
 
 # Include routers from our endpoints
 app.include_router(config_router, prefix="/config", tags=["Config"])
